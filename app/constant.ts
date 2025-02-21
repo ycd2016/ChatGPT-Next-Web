483c3c861787ad1e5ab9e270910932e0b80a5ec1
ycd2016 <1633866047@qq.com>
2025-02-21 11:10:29 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -109,8 +109,8 @@ export const UNFINISHED_INPUT = (id: string) => "unfinished-input-" + id;
 
 export const STORAGE_KEY = "chatgpt-next-web";
 
-export const REQUEST_TIMEOUT_MS = 60000;
-export const REQUEST_TIMEOUT_MS_FOR_THINKING = REQUEST_TIMEOUT_MS * 5;
+export const REQUEST_TIMEOUT_MS = 60000 * 5;
+export const REQUEST_TIMEOUT_MS_FOR_THINKING = 60000 * 5;
 
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
